6a2e6e2dcca7acc1bf12b9fb7afe2bd3e871a695
AbhayGirme <118588885+AbhayGirme@users.noreply.github.com>
2022-11-19 14:13:28 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,14 @@
+n=int(input("Enter no"))
+m1=("Enter first subject")
+m2=("Enter second subject")
+m3=("Enter third subject")
+p=m1+m2+m3/3:
+if n>70 and n<100:
+    print("distinction")
+elif n>60 and n<70:
+    print("first class")
+elif n>50 and n<60:
+    print("pass")
+    print("percnetage")
+    
+    
